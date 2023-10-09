170a84860e8f0bd656fe1c7a96639f2dfd576614
djalmalg <djalma56godinho@gmail.com>
2023-10-09 10:20:31 -0300

    Desenvolvimento do todolist do curso rockseat

---
+++ src/test/java/br/com/dlg/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dlg.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
